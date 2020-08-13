5fa4dfec02fac27d3475e789810fdd41176d5680
Abdel <juniorperezpy@gmail.com>
2020-08-13 17:47:34 -0400

    Add getLocation function

---
+++ src/modules/location.js
@@ -0,0 +1,19 @@
+import PubSub from 'pubsub-js';
+
+const getLocation = () => {
+  let result;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(notifyLocation);
+  } else { 
+    result = Error("Geolocation is not supported by this browser.");
+  }
+
+  return result;
+};
+
+const notifyLocation = (position) => {
+  PubSub.publish('location retrieved', position);
+};
+
+export {getLocation};
