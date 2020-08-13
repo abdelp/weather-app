dae6aead5a13021fccf7e0a0bb3d6464fa62709b
Abdel <juniorperezpy@gmail.com>
2020-08-13 16:38:46 -0400

    Add params provider

---
+++ src/modules/params-provider.js
@@ -0,0 +1,5 @@
+const weatherConf = () => ({api: 'YOUR OPEN WEAHTER MAP API HERE', url: 'https://api.openweathermap.org/data/2.5/weather'});
+
+const imgConf = () => ({api: 'YOUR GIPHY API HERE', url: 'https://api.giphy.com/v1/gifs/translate'});
+
+export {weatherConf, imgConf};
