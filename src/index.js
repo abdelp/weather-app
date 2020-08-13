08fb4407a6d4d41995963d52bc2ff6702a71462c
Abdel <juniorperezpy@gmail.com>
2020-08-13 17:48:29 -0400

    Add seachByCoordinates with the subscription to location retrieved

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as Display from './modules/display.js';
 import './assets/stylesheets/style.css';
 import * as Weather from './modules/weather-provider.js';
 import * as Img from './modules/img-provider.js';
+import * as Location from './modules/location.js';
+import PubSub from 'pubsub-js';
 
 window.searchForecast = async () => {
   const cityName = Display.getVal('city');
@@ -15,3 +17,22 @@ window.searchForecast = async () => {
 
   Display.updateView(weatherData, imgUrl);
 };
+
+const searchByCoordinates = async (event, position) => {
+  let error, weatherData;
+
+  console.log(position);
+  [error, weatherData] = await to(Weather.getForecastByCoordinates({lon: position.coords.longitude, lat: position.coords.latitude}, 'metric'));
+
+  console.log(weatherData);
+  
+  let imgUrl;
+  [error, imgUrl] = await to(Img.getImgUrl(weatherData.description));
+
+  Display.updateView(weatherData, imgUrl);
+};
+
+PubSub.subscribe('location retrieved', searchByCoordinates);
+
+Location.getLocation();
+
