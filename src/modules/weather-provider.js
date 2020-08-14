6861159d1afd177cabe74a16822affef281e0561
Abdel <juniorperezpy@gmail.com>
2020-08-14 17:47:17 -0400

    Add data properties
    -Add data object to store the attributes of the latest result
    -Update name of searchData and searchDataByCoordinates to updateData and
    updateData respectively
    -Add updateUnit to change the temperature and the min and max to the new
    unit
    -Add getData to return all the latest data retrieved

--- src/modules/weather-provider.js
+++ src/modules/weather-provider.js
@@ -1,6 +1,21 @@
 import fetchUrl from './fetcher.js';
 import to from 'await-to-js';
 import {weatherConf} from './params-provider';
+import getCurrentDateTime from './datetime';
+
+let data = {
+  cityName: '',
+  main: '',
+  description: '',
+  units: 'metric',
+  temp: 0,
+  min: 0,
+  max: 0,
+  pressure: 0,
+  humidity: 0,
+  wind: 0,
+  icon: '',
+  lastUpdate: ''};
 
 const formatWeatherData = (data) => {
   const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
@@ -8,17 +23,19 @@ const formatWeatherData = (data) => {
     cityName: data.name,
     main: `weather ${data.weather[0].main} landscape`,
     description: data.weather[0].description,
-    temp: data.main.temp,
-    temp_min: data.main.temp_min,
-    temp_max: data.main.temp_max,
+    temp: parseInt(data.main.temp),
+    temp_min: parseInt(data.main.temp_min),
+    temp_max: parseInt(data.main.temp_max),
     pressure: data.main.pressure,
     humidity: data.main.humidity,
     wind: data.wind.speed,
-    icon: iconUrl
+    icon: iconUrl,
+    units: data.units,
+    lastUpdate: data.lastUpdate
   }
 };
 
-const getForecast = async (cityName, units) => {
+const updateData = async (cityName, units) => {
   const {api, url} = weatherConf();
   const parametrizedUrl = `${url}?q=${cityName}&appid=${api}&units=${units}`;
   let error, result;
@@ -26,10 +43,15 @@ const getForecast = async (cityName, units) => {
   [error, result] = await to(fetchUrl(parametrizedUrl));
   if (error) return error;
 
-  return formatWeatherData(result);
+  result.units = units;
+  result.lastUpdate = getCurrentDateTime();
+
+  data = formatWeatherData(result);
+
+  return data;
 };
 
-const getForecastByCoordinates = async (position, units) => {
+const updateDataByCoordinates = async (position, units) => {
   const {api, url} = weatherConf();
   const parametrizedUrl = `${url}?lat=${position.lat}&lon=${position.lon}&appid=${api}&units=${units}`;
   let error, result;
@@ -37,10 +59,36 @@ const getForecastByCoordinates = async (position, units) => {
   [error, result] = await to(fetchUrl(parametrizedUrl));
   if (error) return error;
 
-  return formatWeatherData(result);
+  result.units = units;
+  result.lastUpdate = getCurrentDateTime();
+
+  data = formatWeatherData(result);
+
+  return data;
 }
 
+const getData = () => {
+  return data;
+};
+
+const updateUnit = (newUnit) => {
+  const {units, temp, temp_min, temp_max} = getData();
+
+  if(newUnit === 'metric' && units != 'metric') {
+    data.temp =  (temp - 32) / 1.8;
+    data.temp_min = (temp_min - 32) / 1.8;
+    data.temp_max = (temp_max - 32) / 1.8;
+  } else if(newUnit === 'imperial' && units != 'imperial') {
+    data.temp = temp * 1.8 + 32;
+    data.temp_min = temp_min * 1.8 + 32;
+    data.temp_max = temp_max * 1.8 + 32;
+  }
+  data.units = newUnit;
+};
+
 export {
-  getForecast,
-  getForecastByCoordinates
+  updateData,
+  updateDataByCoordinates,
+  getData,
+  updateUnit
 };
