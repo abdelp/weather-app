541196dabb92f83bec3cbe7f875580c134ea875e
Abdel <juniorperezpy@gmail.com>
2020-08-14 17:52:05 -0400

    Add units conversion
    -Update searchForecast and searchForecastByCoordinates to updateData and
    updateDataByCoordinates respectively
    -Add updateView method to update the unit of the temperature
    -Add Publishers and subscribers to update the temperature

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import * as Weather from './modules/weather-provider.js';
 import * as Img from './modules/img-provider.js';
 import * as Location from './modules/location.js';
 import PubSub from 'pubsub-js';
-import getCurrentDateTime from './modules/datetime';
 import './assets/stylesheets/toggle-switch.css';
 
 window.searchForecast = async () => {
   const cityName = Display.getVal('city');
+  const units = Display.getUnits();
+
   let error, weatherData;
 
-  [error, weatherData] = await to(Weather.getForecast(cityName, 'metric'));
-
-  weatherData.lastUpdate = getCurrentDateTime();
+  [error, weatherData] = await to(Weather.updateData(cityName, units));
 
   let imgUrl;
   [error, imgUrl] = await to(Img.getImgUrl(weatherData.description));
@@ -23,17 +22,30 @@ window.searchForecast = async () => {
 };
 
 const searchByCoordinates = async (event, position) => {
+  const units = Display.getUnits();
   let error, weatherData;
-
-  [error, weatherData] = await to(Weather.getForecastByCoordinates({lon: position.coords.longitude, lat: position.coords.latitude}, 'metric'));
+  [error, weatherData] = await to(Weather.updateDataByCoordinates({lon: position.coords.longitude, lat: position.coords.latitude}, 'metric'));
 
   let imgUrl;
   [error, imgUrl] = await to(Img.getImgUrl(weatherData.main));
 
-  weatherData.lastUpdate = getCurrentDateTime();
   Display.updateView(weatherData, imgUrl);
 };
 
+const updateView = async (event, unit) => {
+  Weather.updateUnit(unit);
+
+  const {temp, temp_min, temp_max} = Weather.getData();
+
+  Display.updateTemp(temp);
+  Display.updateMinMax({temp_min, temp_max});
+};
+
 PubSub.subscribe('location retrieved', searchByCoordinates);
+PubSub.subscribe('unit changed', updateView);
 
 Location.getLocation();
+
+window.onload = () => {
+  Display.setUnitChangeHandler();
+}
